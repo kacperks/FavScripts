22bf3ef0e5fca611c157adc732e0461f931ccc48
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-02-07 12:45:32 +0100

    Create RobloxInstaller.sh

---
+++ installing/RobloxInstaller.sh
@@ -0,0 +1,9 @@
+sudo dpkg --add-architecture i386​
+
+​wget -nc https://dl.winehq.org/wine-builds/winehq.key​
+
+​sudo apt-key add winehq.key​
+
+​sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'​
+
+​sudo apt update
